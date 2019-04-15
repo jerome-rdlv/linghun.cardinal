d8265d4c068e1cc5b97c1bd054492867dc9e6909
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2019-04-15 10:08:55 +0200

    Correction on video oembed integration (short urls)

--- content/themes/cardinal/partial/flex-video.php
+++ content/themes/cardinal/partial/flex-video.php
@@ -1,6 +1,8 @@
 <?php $theme = CardinalTheme::get_instance() ?>
 <div class="flex-video">
-    <?php $platform = $theme->get_network_key(get_sub_field('video', false)) ?>
+    <?php $oembed = _wp_oembed_get_object() ?>
+    <?php $oembed_data = $oembed->get_data(get_sub_field('video', false)) ?>
+    <?php $platform = $theme->get_network_key($oembed_data->provider_url) ?>
     <?php wp_add_inline_script('tac', '(tarteaucitron.job = tarteaucitron.job || []).push(\'rdlv_'. $platform .'\');') ?>
     <?php $theme->video_html(get_sub_field('video'), $platform .'_player flex-video__video') ?>
 </div>
